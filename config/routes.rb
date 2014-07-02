380f81b9eb43cc6033e0e1042e455f229892a2a6
andy chen <chen.a.andy@gmail.com>
2014-07-02 14:13:37 -0700

    spring causing issues, mongoid initialized, map4rails and geocoder included

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
  get '/' => 'home#login'
+
+ get '/login' => 'sessions#new'
+ post '/login' => 'sessions#create'
+ get '/logout' => 'sessions#destroy'
+
+ get '/new_user' => 'users#new'
+ post '/new_user' => 'users#create'
+
+ 
 end
